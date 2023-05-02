332c3dd3c8390e24dfd1a33bfb4711ddd5fd86f6
tajkir-alam <tajkir.alam@gmail.com>
2023-05-02 13:41:53 +0600

    project setup done

---
+++ src/Providers/RouterProvider.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main></Main>
+    },
+]);
+
+export default router;
